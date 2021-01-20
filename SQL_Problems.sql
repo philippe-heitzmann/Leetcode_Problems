55354c55a2c662acf945875da2a02bac71a45873
philippe-heitzmann <phil062497@gmail.com>
2021-01-19 21:26:22 -0500

    Added new SQL problems

--- SQL_Problems.sql
+++ SQL_Problems.sql
@@ -36,6 +36,20 @@ SELECT
        FROM Employee
        ORDER BY Salary DESC
         LIMIT 1 OFFSET 1),
+
+#177. Nth Highest Salary
+
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  SET N = N-1;
+  RETURN (
+      select distinct Salary 
+      from Employee 
+      order by Salary DESC
+      limit N,1
+  );
+END;r
+
     NULL) AS SecondHighestSalary;
 
 
@@ -63,26 +77,26 @@ from cinema
 where id % 2 != 0 and description not like '%boring%'
 order by rating DESC;
 
-#182. Duplicate Emails
+#182. Duplicate Emails - Easy
 
 select distinct Email from Person
 group by Email
 having count(Email) > 1;
 
 
-#181. Employees Earning More Than Their Managers
+#181. Employees Earning More Than Their Managers - Easy
 
 select x.Name as Employee 
 from Employee x join Employee y on x.ManagerId = y.Id
 where x.Salary > y.Salary;
 
-#183. Customers Who Never Order
+#183. Customers Who Never Order - Easy
 
 select Name as Customers
 from Customers left join Orders on Customers.Id=Orders.CustomerId
 where CustomerId IS NULL;
 
-#596. Classes More Than 5 Students
+#596. Classes More Than 5 Students - Easy
 
 select class 
 from courses
@@ -90,3 +104,65 @@ group by class
 having count(distinct student) >= 5;
 
 
+#197. Rising Temperature - Easy
+
+SELECT
+    weather.id AS 'Id'
+FROM
+    weather
+        JOIN
+    weather w ON DATEDIFF(weather.recordDate, w.recordDate) = 1
+        AND weather.Temperature > w.Temperature;
+
+#196. Delete Duplicate Emails - Easy
+
+delete p1 from Person p1, Person p2
+where p1.id > p2.id and p1.Email = p2.Email;
+
+#1112. Highest Grade For Each Student - Medium
+
+select student_id, course_id, grade 
+from(
+Select *,
+    rank() over(partition by student_id order by grade DESC, course_id ASC) as rank1
+    from Enrollments)
+    as t2
+    where rank1 = 1;
+
+#1398. Customers Who Bought Products A and B but Not C - Medium
+
+select Orders.customer_id, customer_name
+from Orders join Customers on Orders.customer_id = Customers.customer_id
+group by Orders.customer_id
+having sum(case when product_name = 'A' then 1 else 0 end) > 0 and sum(case when product_name = 'B' then 1 else 0 end) > 0 and sum(case when product_name = 'C' then 1 else 0 end) = 0;
+
+#570. Managers with at Least 5 Direct Reports - Medium
+
+select name 
+from Employee
+where Id in(
+select ManagerId 
+    from Employee
+    group by ManagerId
+    having count(*) >= 5
+);
+
+#1045. Customers Who Bought All Products - Medium
+
+select customer_id
+from Customer
+group by customer_id  
+having count(distinct product_key) >= (select count(distinct product_key) from product);
+
+#1421. NPV Queries - Medium
+
+select q.id, q.year, ifnull(n.npv, 0) as NPV
+from Queries q left join NPV n on q.id = n.id and q.year = n.year;
+
+
+# 1350. Students With Invalid Departments - Easy
+
+select id, name 
+from Students
+where department_id not in(
+select id from Departments);
